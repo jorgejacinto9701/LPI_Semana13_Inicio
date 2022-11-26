5370bbbd238b40174cc27b886074713aa20527e7
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2022-11-26 07:43:57 -0500

    Reportes gráficos

---
+++ src/gui/reporte/grafico/FrmReporteGraficoTortas.java
@@ -0,0 +1,163 @@
+package gui.reporte.grafico;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Date;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+
+import entidad.ReporteEntidad;
+import model.ReporteModel;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.swing.JRViewer;
+import util.FechaUtil;
+import util.GeneradorReporte;
+import util.Validaciones;
+
+public class FrmReporteGraficoTortas extends JFrame implements ActionListener   {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField txtInicio;
+	private JTextField txtFin;
+	private JButton btnFiltrar;
+	private JPanel panelReporte;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UIManager.setLookAndFeel("com.jtattoo.plaf.smart.SmartLookAndFeel");
+					FrmReporteGraficoTortas frame = new FrmReporteGraficoTortas();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FrmReporteGraficoTortas() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1206, 714);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblTitulo = new JLabel("Reporte de Torta");
+		lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitulo.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblTitulo.setBounds(10, 26, 1161, 36);
+		contentPane.add(lblTitulo);
+		
+		panelReporte = new JPanel();
+		panelReporte.setBorder(new TitledBorder(null, "Reportes", TitledBorder.CENTER, TitledBorder.TOP, null, null));
+		panelReporte.setBounds(31, 176, 1144, 475);
+		contentPane.add(panelReporte);
+		panelReporte.setLayout(new BorderLayout());
+		
+		JLabel lblFechaInicio = new JLabel("Fecha Inicio");
+		lblFechaInicio.setBounds(31, 126, 81, 14);
+		contentPane.add(lblFechaInicio);
+		
+		txtInicio = new JTextField();
+		txtInicio.setColumns(10);
+		txtInicio.setBounds(131, 123, 119, 20);
+		contentPane.add(txtInicio);
+		
+		JLabel lblFechaFin = new JLabel("Fecha Fin");
+		lblFechaFin.setBounds(322, 126, 90, 14);
+		contentPane.add(lblFechaFin);
+		
+		txtFin = new JTextField();
+		txtFin.setColumns(10);
+		txtFin.setBounds(422, 123, 109, 20);
+		contentPane.add(txtFin);
+		
+		btnFiltrar = new JButton("Filtrar");
+		btnFiltrar.addActionListener(this);
+		btnFiltrar.setBounds(592, 122, 162, 23);
+		contentPane.add(btnFiltrar);
+		
+		
+	}	
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnFiltrar) {
+			actionPerformedBtnFiltrarJButton(e);
+		}
+	}
+	protected void actionPerformedBtnFiltrarJButton(ActionEvent e) {
+		String fecIni  = txtInicio.getText().trim();
+		String fecFin  = txtFin.getText().trim();
+		
+		if (!fecIni.matches(Validaciones.FECHA)) {
+			mensaje("La fecha Inicio tiene formato yyyy-MM-dd");
+		}else if (!fecFin.matches(Validaciones.FECHA)) {
+			mensaje("La fecha Fin tiene formato yyyy-MM-dd");
+		}else if (FechaUtil.isNotSuperiorFechaYYYYMMdd(fecIni, fecFin)) {
+			mensaje("La Fecha fin es superior a la Fecha inicio");
+		}else {
+			Date dtIni = Date.valueOf(fecIni);
+			Date dtFin = Date.valueOf(fecFin);
+			
+			ReporteModel model = new ReporteModel();
+			List<ReporteEntidad> lista = model.listaPorRangoFechas(dtIni, dtFin);
+			
+			//Datos del reporte
+			JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(lista);
+			
+			//Diseño del reporte
+			String jasper = "reporteXXX.jasper";	
+			
+			//Se obtiene el reporte
+			JasperPrint print = GeneradorReporte.genera(jasper, dataSource, null);
+			
+			JRViewer jRViewer = new JRViewer(print);
+			
+			panelReporte.removeAll();
+			panelReporte.add(jRViewer);
+			panelReporte.repaint();
+			panelReporte.revalidate();
+			
+		}	
+	
+	}
+	
+	public void mensaje(String ms){
+		JOptionPane.showMessageDialog(this, ms);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/model/ReporteModel.java
@@ -0,0 +1,57 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import entidad.ReporteEntidad;
+import util.MySqlDBConexion;
+
+public class ReporteModel {
+
+	private static Logger log = Logger.getLogger(ReporteModel.class.getName());
+	
+	public List<ReporteEntidad> listaPorRangoFechas(Date fecIni, Date fecFin){
+		ArrayList<ReporteEntidad> salida = new ArrayList<ReporteEntidad>();
+		
+		Connection conn = null;
+		PreparedStatement psmt = null;
+		ResultSet rs = null;
+		try {
+			//1 Se crea la conexion
+			conn = MySqlDBConexion.getConexion();
+			
+			//2 Se prepara el SQL
+			String sql = "select g.nombre, count(*) from director d inner join grado g on d.idgrado = g.idgrado where d.fechaNacimiento between ? and ? group by g.nombre order by 1 asc"; 
+			psmt = conn.prepareStatement(sql);
+			psmt.setDate(1, fecIni);
+			psmt.setDate(2, fecFin);
+			
+			log.info(">>> " + psmt);
+			
+			//3 Se ejecuta el SQL en la base de datos
+			rs = psmt.executeQuery();
+			ReporteEntidad obj = null;
+			while(rs.next()) {
+				obj = new ReporteEntidad();
+				obj.setCategoria(rs.getString(1));
+				obj.setCantidad(rs.getInt(0));
+				salida.add(obj);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null) rs.close();
+				if (psmt != null) psmt.close();
+				if (conn != null) conn.close();
+			} catch (Exception e2) {}
+		}
+		return salida;
+	}
+
+}
